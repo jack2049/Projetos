5419d270160e783f8d52399a14d87b86468c923f
jack2049 <jaquelinedias2022@gmail.com>
2023-03-28 18:09:20 -0300

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+
+function corroxo() {
+    document.getElementById("v-pills-home-tab").style.background = "#7d5b8c";
+    document.getElementById("v-pills-messages-tab").style.background = "#7d5b8c";
+    
+    document.getElementById("v-pills-profile-tab").style.background = "#7d5b8c";
+    document.getElementById("v-pills-settings-tab").style.background = "#7d5b8c";
+
+    
+
+
+
+}
+
+
+
+function corazul() {
+    document.getElementById("v-pills-home-tab").style.background = "#4169e1";
+    document.getElementById("v-pills-messages-tab").style.background = "#4169e1";
+    
+    document.getElementById("v-pills-profile-tab").style.background =" #4169e1";
+    document.getElementById("v-pills-settings-tab").style.background = "#4169e1";
+
+
+}
+
+
+
+function corlaranja() {
+    document.getElementById("v-pills-home-tab").style.background = "#d75413";
+    document.getElementById("v-pills-messages-tab").style.background = "#d75413";
+    
+    document.getElementById("v-pills-profile-tab").style.background = "#d75413";
+    document.getElementById("v-pills-settings-tab").style.background = "#d75413";
+
+
+}
+
+
+
+
+function botao() {
+    var element = document.getElementById("minhaDIV");
+    element.classList.toggle("ocultar");
+ }
